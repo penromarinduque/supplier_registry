8cebd6697b61ba5a60d76bb6be77cfde3ae1eca3
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2025-09-25 11:37:56 +0800

    Enhancethe welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,13 +104,9 @@
     <div class="container py-5">
         <!-- Logo Section -->
         <div class="logo-container text-center mb-5">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-md-4">
-                    <img src="{{ asset('LOGO.png')}}" alt="DENR Logo" class="img-fluid mb-3">
-                </div>
-                <div class="col-md-4">
-                    <img src="{{ asset('Bagong_Pilipinas_logo.png')}}" alt="Bagong Pilipinas Logo" class="img-fluid mb-3">
-                </div>
+            <div class="row justify-content-center">
+                <img src="{{ asset('LOGO.png')}}" alt="DENR Logo" class="img-fluid mb-3">
+                <img src="{{ asset('Bagong_Pilipinas_logo.png')}}" alt="Bagong Pilipinas Logo" class="img-fluid mb-3">
             </div>
             <h4 class="logo-text mb-2">Department of Environment and Natural Resources</h4>
             <h5 class="logo-text mb-4">PENRO MARINDUQUE</h5>
