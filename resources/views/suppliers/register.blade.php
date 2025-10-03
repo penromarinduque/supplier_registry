107d43f0ec4db3366955c2127ca34797ba855c61
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2025-10-03 15:55:45 +0800

    Change the name and Title and enhance the form

--- resources/views/suppliers/register.blade.php
+++ resources/views/suppliers/register.blade.php
@@ -19,63 +19,63 @@
                 @endforeach --}}
                 <h6 class="font-weight-bold h5">Company Information</h6>
                 <div class="row align-items-end mb-3">
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="company_name">Company Name <span class="text-danger">*</span></label>
                         <input type="text" name="company_name" id="company_name" class="form-control" value="{{ old('company_name') }}" required>
                         @error('company_name', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="philgeps_reg_no">PHILGEPS Registration/Certificate Number <span class="text-danger">*</span></label>
                         <input type="text" name="philgeps_reg_no" id="philgeps_reg_no" class="form-control" value="{{ old('philgeps_reg_no') }}" required>
                         @error('philgeps_reg_no', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="philgeps_validity">Validity of PHILGEPS Certificate if Platinum Member <span class="text-danger">*</span></label>
                         <input type="date" name="philgeps_validity" id="philgeps_validity" class="form-control" value="{{ old('philgeps_validity') }}" required>
                         @error('philgeps_validity', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="business_permit_no">Business/Mayor's Permit Number <span class="text-danger">*</span></label>
                         <input type="text" name="business_permit_no" id="business_permit_no" class="form-control" value="{{ old('business_permit_no') }}" required>
                         @error('business_permit_no', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="business_permit_validity">Validity of Business/Mayor's Permit <span class="text-danger">*</span></label>
                         <input type="date" name="business_permit_validity" id="business_permit_validity" class="form-control" value="{{ old('business_permit_validity') }}" required>
                         @error('business_permit_validity', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="line_of_business">Line of Business <span class="text-danger">*</span></label>
                         <input type="text" name="line_of_business" id="line_of_business" class="form-control" value="{{ old('line_of_business') }}" required>
                         @error('line_of_business', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="philgeps_cert">Upload PHILGEPS Registration Certificate <span class="text-danger">*</span></label>
                         <input type="file" accept="image/*" name="philgeps_cert" id="philgeps_cert" class="form-control-file" required>
                         @error('philgeps_cert', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="business_permit">Upload Valid and Current Business/Mayor's Permit <span class="text-danger">*</span></label>
                         <input type="file" accept="image/*" name="business_permit" id="business_permit" class="form-control-file" required>
                         @error('business_permit', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="bir_cert">Upload BIR Certificate of Registration/DTI Permit <span class="text-danger">*</span></label>
                         <input type="file" accept="image/*" name="bir_cert" id="bir_cert" class="form-control-file" required>
                         @error('bir_cert', 'store')
@@ -87,7 +87,7 @@
                 <h6 class="font-weight-bold h5">Authorized Representative</h6>
                 <div class="row align-items-end">
                     @csrf
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="email">Email <span class="text-danger">*</span></label>
                         <input type="text" name="email" id="email" class="form-control" value="{{ old('email') }}" required>
                         @error('email', 'store')
@@ -95,28 +95,28 @@
                         @enderror
                     </div>
                     
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="authorized_representative">Authorized Representative <span class="text-danger">*</span></label>
                         <input type="text" name="authorized_representative" id="authorized_representative" class="form-control" value="{{ old('authorized_representative') }}" required>
                         @error('authorized_representative', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="landline_no">Landline No. <span class="text-danger">*</span></label>
                         <input type="text" name="landline_no" id="landline_no" class="form-control" value="{{ old('landline_no') }}" required>
                         @error('landline_no', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="mobile_no">Mobile No. <span class="text-danger">*</span></label>
                         <input type="text" name="mobile_no" id="mobile_no" class="form-control" value="{{ old('mobile_no') }}" required>
                         @error('mobile_no', 'store')
                             <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="col col-md-6 col-lg-4 mb-2">
+                    <div class="col-12 col-md-6 col-lg-4 mb-2">
                         <label for="valid_id">Upload Valid ID of Authorized Representative <span class="text-danger">*</span></label>
                         <input type="file" accept="image/*" name="valid_id" id="valid_id" class="form-control-file" required>
                         @error('valid_id', 'store')
